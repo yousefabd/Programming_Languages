76732b271276d13050099503d8154790dcbb2ae5
LEGION <LEGION@YOUSEF>
2023-11-12 21:35:40 +0200

    Used safe area (without status bar overlay)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:prog_languages/screens/login.dart';
 
@@ -6,6 +7,9 @@ import 'package:prog_languages/screens/login.dart';
 final kColorScheme =
     ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 27, 127, 72));
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(statusBarColor: kColorScheme.onTertiaryContainer),
+  );
   runApp(
     MaterialApp(
       theme: ThemeData().copyWith(
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -11,109 +11,111 @@ class LogInScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: Stack(
-        children: [
-          SvgPicture.asset(
-            'assets/images/svborders.svg', // new imge added here, got changed a little bit, but the size now is better, and the quiality ofc.
-            fit: BoxFit.fill,
-          ),
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text(
-                  "Hello!",
-                  style: GoogleFonts.bitter(
-                    color: Theme.of(context).colorScheme.onBackground,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 65,
-                  ),
-                ),
-                Text(
-                  "Sign in to your account",
-                  style: GoogleFonts.bitter(
-                    color: Theme.of(context).colorScheme.onBackground,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(
-                  height: 40,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 40, vertical: 20),
-                  child: TextField(
-                    keyboardType: TextInputType.number,
-                    maxLength: 10,
-                    controller: _numberController,
-                    decoration: const InputDecoration(
-                      label: Row(mainAxisSize: MainAxisSize.min,children: [
-                        Icon(Icons.phone_android),
-                        SizedBox(width: 8),
-                        Text('Phone Number')
-                      ]),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(30),
-                        ),
-                      ),
+      body: SafeArea(
+        child: Stack(
+          children: [
+            SvgPicture.asset(
+              'assets/images/svborders.svg', // new imge added here, got changed a little bit, but the size now is better, and the quiality ofc.
+              fit: BoxFit.fill,
+            ),
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    "Hello!",
+                    style: GoogleFonts.bitter(
+                      color: Theme.of(context).colorScheme.onBackground,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 65,
                     ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 40, vertical: 20),
-                  child: TextField(
-                    controller: _passwordController,
-                    decoration: const InputDecoration(
-                      label: Row(mainAxisSize: MainAxisSize.min,children: [
-                        Icon(Icons.lock),
-                        SizedBox(width: 8),
-                        Text('Password')
-                      ]),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(30),
-                        ),
-                      ),
+                  Text(
+                    "Sign in to your account",
+                    style: GoogleFonts.bitter(
+                      color: Theme.of(context).colorScheme.onBackground,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                ),
-                SizedBox(
-                  width: 280, 
-                  height: 100, 
-                  child: Padding(
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  Padding(
                     padding: const EdgeInsets.symmetric(
                         horizontal: 40, vertical: 20),
-                    child: ElevatedButton(
-                      onPressed: () {
-                        //
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Color.fromARGB(255, 49, 237, 237), 
-                        foregroundColor: Colors.black, 
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30),
-                          side: const BorderSide(
-                            color: Color.fromARGB(255, 49, 237, 237), 
+                    child: TextField(
+                      keyboardType: TextInputType.number,
+                      maxLength: 10,
+                      controller: _numberController,
+                      decoration: const InputDecoration(
+                        label: Row(mainAxisSize: MainAxisSize.min, children: [
+                          Icon(Icons.phone_android),
+                          SizedBox(width: 8),
+                          Text('Phone Number')
+                        ]),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(30),
                           ),
                         ),
                       ),
-                      child: Text(
-                        'Sign In',
-                        style: GoogleFonts.bitter(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 22,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 40, vertical: 20),
+                    child: TextField(
+                      controller: _passwordController,
+                      decoration: const InputDecoration(
+                        label: Row(mainAxisSize: MainAxisSize.min, children: [
+                          Icon(Icons.lock),
+                          SizedBox(width: 8),
+                          Text('Password')
+                        ]),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(30),
+                          ),
                         ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                  SizedBox(
+                    width: 280,
+                    height: 100,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 40, vertical: 20),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          //
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color.fromARGB(255, 49, 237, 237),
+                          foregroundColor: Colors.black,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30),
+                            side: const BorderSide(
+                              color: Color.fromARGB(255, 49, 237, 237),
+                            ),
+                          ),
+                        ),
+                        child: Text(
+                          'Sign In',
+                          style: GoogleFonts.bitter(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 22,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
