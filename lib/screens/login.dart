07445327d03f84f3195fbb4661e3c11c406dcdc9
LEGION <LEGION@YOUSEF>
2023-11-12 02:34:11 +0200

    Added a border image asset for Log-in Screen

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -8,53 +8,48 @@ class LogInScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Medical Bay',
-            style: GoogleFonts.croissantOne(color: Colors.white)),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 60),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            TextField(
-              controller: _numberController,
-              keyboardType: TextInputType.number,
-              maxLength: 10,
-              decoration: const InputDecoration(
-                  label: Text('Phone Number'), border: OutlineInputBorder()),
-            ),
-            const SizedBox(height: 30),
-            TextField(
-              controller: _passwordController,
-              decoration: const InputDecoration(
-                  label: Text('Password'), border: OutlineInputBorder()),
-            ),
-            Container(
-              padding: const EdgeInsets.only(left: 180),
-              child: TextButton(
-                onPressed: () {},
-                child: const Text(
-                  'Forgot Password?',
-                  textAlign: TextAlign.left,
-                ),
-              ),
-            ),
-            Container(
-                height: 60,
-                width: 120,
-                child: ElevatedButton(
-                    onPressed: () {
-                      print(_numberController.text);
-                      print(_passwordController.text);
-                    },
-                    child: Text(
-                      'LOG IN',
-                      style: TextStyle(fontSize: 25),
-                    )))
-          ],
+      body: Stack(children: [
+        Container(
+          foregroundDecoration: BoxDecoration(
+            image: DecorationImage(
+                image: Image.asset('assets/images/login_border.png').image,
+                fit: BoxFit.fill),
+          ),
         ),
-      ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 60),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: _numberController,
+                keyboardType: TextInputType.number,
+                maxLength: 10,
+                decoration: const InputDecoration(
+                    label: Text('Phone Number'), border: OutlineInputBorder()),
+              ),
+              const SizedBox(height: 30),
+              TextField(
+                controller: _passwordController,
+                decoration: const InputDecoration(
+                    label: Text('Password'), border: OutlineInputBorder()),
+              ),
+              SizedBox(
+                  height: 60,
+                  width: 120,
+                  child: ElevatedButton(
+                      onPressed: () {
+                        print(_numberController.text);
+                        print(_passwordController.text);
+                      },
+                      child: const Text(
+                        'LOG IN',
+                        style: TextStyle(fontSize: 25),
+                      )))
+            ],
+          ),
+        ),
+      ]),
     );
   }
 }
