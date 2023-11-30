ba450ccece5e1eac320bda1771a830b3eb21962c
LEGION <LEGION@YOUSEF>
2023-11-30 13:37:57 +0200

    added dummy list for orders

---
+++ lib/data/requested_orders_list.dart
@@ -0,0 +1,50 @@
+final List<Map<String, dynamic>> requestedOrders = [
+  {
+    "orderState": 0, //pending
+    "orderTitle": "My Order",
+  },
+  {
+    "orderState": 1, //sent
+    "orderTitle": "My Second Order",
+  },
+  {
+    "orderState": 2, //recieved
+    "orderTitle": "My Third Order",
+  },
+  {
+    "orderState": 0, //pending
+    "orderTitle": "My Order",
+  },
+  {
+    "orderState": 1, //sent
+    "orderTitle": "My Second Order",
+  },
+  {
+    "orderState": 2, //recieved
+    "orderTitle": "My Third Order",
+  },
+  {
+    "orderState": 0, //pending
+    "orderTitle": "My Order",
+  },
+  {
+    "orderState": 1, //sent
+    "orderTitle": "My Second Order",
+  },
+  {
+    "orderState": 2, //recieved
+    "orderTitle": "My Third Order",
+  },
+  {
+    "orderState": 0, //pending
+    "orderTitle": "My Order",
+  },
+  {
+    "orderState": 1, //sent
+    "orderTitle": "My Second Order",
+  },
+  {
+    "orderState": 2, //recieved
+    "orderTitle": "My Third Order",
+  },
+];
